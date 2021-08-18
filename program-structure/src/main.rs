67c19904f3a1c38b6da30039c8eae86d4afc23da
Chan Kang <me@kentakang.com>
2021-08-18 15:27:10 +0900

    feat: add program structure

---
+++ program-structure/src/main.rs
@@ -0,0 +1,8 @@
+// This is main function
+fn main() {
+    // Code for the function body
+    println!("Hello, world!"); // println! macro, is predefined in Rust.
+
+    // Call println! with three arguments
+    println!("The first letter of the English alphabet is {} and the last letter is {}.", 'A', 'Z');
+}
